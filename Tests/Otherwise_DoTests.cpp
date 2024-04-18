2c8848c42dd281e9d2d7d29ac0470f10222f0a59
Neko Box Coder <nekoboxcoder@gmail.com>
2024-04-18 03:07:05 +0100

    Otherwise_Do tests

--- Tests/Otherwise_DoTests.cpp
+++ Tests/Otherwise_DoTests.cpp
@@ -1,7 +1,8 @@
 #include "CppOverride.hpp"
 #include "ssTest.hpp"
-#include "./FileFunctions.hpp"
-#include "./ClassFunctions.hpp"
+#include "./Components/FileFunctions.hpp"
+#include "./Components/TestClasses.hpp"
+#include "./Components/Shapes.hpp"
 
 CppOverride::Overrider OverrideObj;
 
@@ -13,11 +14,14 @@ int main()
         OverrideObj = CppOverride::Overrider();
     };
     
-    ssTEST("Returns Test")
+    ssTEST("WhenCalledWith Condition Not Met Should Trigger Otherwise_Do For Return")
     {
-        Rectangle rect(1.5, 1.5);
+        ssTEST_OUTPUT_SETUP
+        (
+            Rectangle rect(1.5, 1.5);
+            bool otherwiseDoCalled = false;
+        );
         
-        bool otherwiseDoCalled = false;
         
         CO_SETUP_OVERRIDE   (rect, GetWidth)
                             .WhenCalledWith(2.f)
@@ -30,18 +34,67 @@ int main()
                                 }
                             );
         
-        rect.GetWidth(2.f);
+        ssTEST_OUTPUT_EXECUTION
+        (
+            rect.GetWidth(2.f);
+        );
         ssTEST_OUTPUT_ASSERT(otherwiseDoCalled == false);
         
-        rect.GetWidth(3.f);
+        ssTEST_OUTPUT_EXECUTION
+        (
+            rect.GetWidth(3.f);
+        );
         ssTEST_OUTPUT_ASSERT(otherwiseDoCalled == true);
     };
     
-    ssTEST("SetArgs Test")
+    ssTEST("If Condition Not Met Should Trigger Otherwise_Do For Return")
     {
-        bool otherwiseDoCalled = false;
+        ssTEST_OUTPUT_SETUP
+        (
+            Rectangle rect(1.5, 1.5);
+            bool otherwiseDoCalled = false;
+        );
         
-        CO_SETUP_OVERRIDE   (OverrideObj, FuncWithConstArgsAndArgsToSet)
+        
+        CO_SETUP_OVERRIDE   (rect, GetWidth)
+                            .If
+                            (
+                                [] (const std::vector<void *>& args)
+                                {
+                                    return (*(float*)args[0] == 2.f);
+                                }
+                            )
+                            .Returns<float>(6.f)
+                            .Otherwise_Do
+                            (
+                                [&otherwiseDoCalled] (const std::vector<void *>& args)
+                                {
+                                    otherwiseDoCalled = true;
+                                }
+                            );
+        
+        ssTEST_OUTPUT_EXECUTION
+        (
+            rect.GetWidth(2.f);
+        );
+        ssTEST_OUTPUT_ASSERT(otherwiseDoCalled == false);
+        
+        ssTEST_OUTPUT_EXECUTION
+        (
+            rect.GetWidth(3.f);
+        );
+        ssTEST_OUTPUT_ASSERT(otherwiseDoCalled == true);
+    };
+    
+    ssTEST("WhenCalledWith Condition Not Met Should Trigger Otherwise_Do For SetArgs")
+    {
+        ssTEST_OUTPUT_SETUP
+        (
+            bool otherwiseDoCalled = false;
+            std::string testString;
+        );
+        
+        CO_SETUP_OVERRIDE   (OverrideObj, ConstArgsAndArgsToSetFunc)
                             .WhenCalledWith(1, 2.f, CO_ANY)
                             .SetArgs<   CO_ANY_TYPE, 
                                         CO_ANY_TYPE, 
@@ -56,12 +109,58 @@ int main()
                                 }
                             );
 
-        std::string testString;
-        
-        ConstArgsAndArgsToSetFunc(1, 2.f, testString);
+        ssTEST_OUTPUT_EXECUTION
+        (
+            CppOverrideTest::Const::ConstArgsAndArgsToSetFunc(1, 2.f, testString);
+        );
         ssTEST_OUTPUT_ASSERT(otherwiseDoCalled == false);
         
-        ConstArgsAndArgsToSetFunc(2, 3.f, testString);
+        ssTEST_OUTPUT_EXECUTION
+        (
+            CppOverrideTest::Const::ConstArgsAndArgsToSetFunc(2, 3.f, testString);
+        );
+        ssTEST_OUTPUT_ASSERT(otherwiseDoCalled == true);
+    };
+    
+    ssTEST("If Condition Not Met Should Trigger Otherwise_Do For SetArgs")
+    {
+        ssTEST_OUTPUT_SETUP
+        (
+            bool otherwiseDoCalled = false;
+            std::string testString;
+        );
+        
+        CO_SETUP_OVERRIDE   (OverrideObj, ConstArgsAndArgsToSetFunc)
+                            .If
+                            (
+                                [] (const std::vector<void *>& args)
+                                {
+                                    return (*(int*)args[0] == 1) && (*(float*)args[1] == 2.f);
+                                }
+                            )
+                            .SetArgs<   CO_ANY_TYPE, 
+                                        CO_ANY_TYPE, 
+                                        std::string&>(  CO_DONT_SET, 
+                                                        CO_DONT_SET, 
+                                                        "test")
+                            .Otherwise_Do
+                            (
+                                [&otherwiseDoCalled] (const std::vector<void *>& args)
+                                {
+                                    otherwiseDoCalled = true;
+                                }
+                            );
+
+        ssTEST_OUTPUT_EXECUTION
+        (
+            CppOverrideTest::Const::ConstArgsAndArgsToSetFunc(1, 2.f, testString);
+        );
+        ssTEST_OUTPUT_ASSERT(otherwiseDoCalled == false);
+        
+        ssTEST_OUTPUT_EXECUTION
+        (
+            CppOverrideTest::Const::ConstArgsAndArgsToSetFunc(2, 3.f, testString);
+        );
         ssTEST_OUTPUT_ASSERT(otherwiseDoCalled == true);
     };
     
